33abdf5776da06ecc479af257b3d24c0f2b3bb50
Jeffrey Salmond <js947@cam.ac.uk>
2018-12-01 17:59:22 +0000

    day 1 part 2

---
+++ 1/2.py
@@ -0,0 +1,25 @@
+from __future__ import print_function
+
+def run(l):
+    seen = [0]
+    current = 0
+    while True:
+        for i in l:
+            current += i
+            if current in seen:
+                return current
+            seen.append(current)
+        print("cycle", current, len(seen))
+
+t1 = [+1, -1]
+t2 = [+3, +3, +4, -2, -4]
+t3 = [-6, +3, +8, +5, -6]
+t4 = [+7, +7, -2, -7, -4]
+
+real = [int(i) for i in open('input', 'r')]
+
+print("t1", run(t1), 0)
+print("t2", run(t2), 10)
+print("t3", run(t3), 5)
+print("t4", run(t4), 14)
+print("real", run(real))
