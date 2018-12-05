e3691f7d56e4b956de78e199b1a5bfdf1621ac91
Jeffrey Salmond <js947@cam.ac.uk>
2018-12-05 21:53:37 +0000

    day 5 add a second method and compare

--- 5/1.py
+++ 5/1.py
@@ -1,9 +1,10 @@
 from __future__ import print_function
+import string
 
 def read(filename):
     return open(filename, 'r').read().strip()
 
-def react(s):
+def react1(s):
     buf = []
     for c in s:
         if buf and c == buf[-1].swapcase():
@@ -12,11 +13,25 @@ def react(s):
             buf.append(c)
     return len(buf)
 
-print(react(read('test.in')))
-print(react(read('puzzle.in')))
+def react2(s):
+    lower = sorted(set(c.lower() for c in s))
+    upper = sorted(set(c.upper() for c in s))
+    pairs = [a+b for a, b in zip(lower, upper) + zip(upper, lower)]
 
-def best_remove(s):
-    return min(react([cc for cc in s if cc.lower() != c]) for c in set(c.lower() for c in s))
+    while any(p in s for p in pairs):
+        for p in pairs:
+            s = s.replace(p, "")
+    return len(s)
 
-print(best_remove(read('test.in')))
-print(best_remove(read('puzzle.in')))
+for react in (react1, react2):
+    print(react(read('test.in')))
+    print(react(read('puzzle.in')))
+
+    def best_remove(s):
+        return min(react(s.replace(c,"").replace(c.upper(),"")) for c in set(c.lower() for c in s))
+        return min(react([cc for cc in s if cc.lower() != c]) for c in set(c.lower() for c in s))
+
+    print(best_remove(read('test.in')))
+    print(best_remove(read('puzzle.in')))
+
+    print()
